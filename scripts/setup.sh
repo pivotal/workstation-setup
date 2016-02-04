d5be7307bf706ca8c1153dc342db68a70ca0d036
Grant Hutchins <github@nertzy.com>
2016-02-04 15:10:06 -0600

    Use the main Bash-it git repo
    Seems like we were using a fork

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -44,7 +44,7 @@ cp files/dircolors.ansi-dark ~/.dircolors
 cp files/.inputrc ~/.inputrc
 rm -rf ~/.bash_it
 pushd ~/
-git clone https://github.com/ahmadassaf/bash-it.git ~/.bash_it
+git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 popd
 
