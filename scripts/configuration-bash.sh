b4758829544294c54d0cf90224ec2e4993472c86
Todd Sedano <professor@gmail.com>
2016-05-25 15:24:34 -0700

    Adds who is paired to the command prompt

--- scripts/configuration-bash.sh
+++ scripts/configuration-bash.sh
@@ -7,5 +7,6 @@ cp files/.inputrc ~/.inputrc
 rm -rf ~/.bash_it
 pushd ~/
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
+cp files/add_user_initials_to_git_prompt_info.bash ~/.bash_it/custom
 ~/.bash_it/install.sh
 popd
