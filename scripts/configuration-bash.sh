4cc4123e7a98f49de299840f8c7fbabf2d6337ef
Todd Sedano <professor@gmail.com>
2016-05-26 10:30:46 -0700

    Adds git completion

--- scripts/configuration-bash.sh
+++ scripts/configuration-bash.sh
@@ -9,4 +9,6 @@ pushd ~/
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
 popd
 cp files/add_user_initials_to_git_prompt_info.bash ~/.bash_it/custom
+mkdir -p ~/.bash_it/completion/enabled
+cp ~/.bash_it/completion/available/git.completion.bash ~/.bash_it/completion/enabled/git.completion.bash
 ~/.bash_it/install.sh
