4d63c5685551e8db502b20c8d41df5ba1eeb3f86
Todd Sedano <professor@gmail.com>
2016-05-26 13:13:19 -0700

    Stop Photos from opening automatically (#41)
    * Stop Photos from opening automatically
    
    - very handy on an iOS project or when someone wants to charge their phone.
    
    * Includes directions on how to revert

--- scripts/configuration-osx.sh
+++ scripts/configuration-osx.sh
@@ -17,3 +17,8 @@ defaults write /Users/pivotal/Library/Preferences/.GlobalPreferences InitialKeyR
 # set finder to display full path in title bar
 defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
 
+# stop Photos from opening automatically
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+#to revert use defaults -currentHost delete com.apple.ImageCapture disableHotPlug
+
+
