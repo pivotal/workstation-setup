078eeda92a529702ca3d047f1c3738cec2e41780
Todd Sedano <professor@gmail.com>
2016-05-27 16:16:48 -0700

    Brings in classic bobby pivotal theme
    - I'm not sure how to easily modify the .bash_profile's export BASH_IT_THEME='bobby' from the commandline

---
+++ files/bobby_pivotal/bobby_pivotal.theme.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+# This Pivotal's customization of the bobby theme
+
+function prompt_command() {
+    PS1="\n$(battery_char) ${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
+}
+
+PROMPT_COMMAND=prompt_command;
