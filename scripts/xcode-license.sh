055ef412a3ecd607e86d824e0a8ac7ba82203d1e
Ashraf Hanafy <ahanafy@pivotal.io>
2016-06-15 16:29:53 -0400

    use accept option for xcodebuild

--- scripts/xcode-license.sh
+++ scripts/xcode-license.sh
@@ -1,7 +1,5 @@
 echo
-echo "You need to agree to the Xcode Software License Agreement"
-echo "You will need to enter your password first."
-echo "Then, when the agree comes up, press 'q' to jump to the end and then type 'agree' and press return"
+echo "Accepting the XCode Software License Agreement"
 echo
 
-sudo xcodebuild -license
+sudo xcodebuild -license accept
