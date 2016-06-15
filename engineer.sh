c9447bb8a14709136ca673bfa7ab35ca4617739a
Ashraf Hanafy <ahanafy@pivotal.io>
2016-06-15 14:13:08 -0400

    Revert "Merge pull request #54 from pivotal/no-more-prompts"
    This reverts commit 95436d0cb1e213edacd4c58888ff314cfec68e41, reversing
    changes made to dd8ee5e8c6826600526bbc0cc253c34705a25730.

--- engineer.sh
+++ engineer.sh
@@ -5,10 +5,5 @@ set -e
 
 SCRIPTS_DIR="$(dirname "$0")/scripts"
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root, use sudo "$0" instead" 1>&2
-   exit 1
-fi
-
 ${SCRIPTS_DIR}/setup.sh engineer
 
