62240a6e57ef58790408083df6f3467e3842c963
cjameson@pivotal.io <cjameson@pivotal.io>
2017-04-17 09:44:33 -0700

    move ruby and node to labs-only

--- scripts/applications-labs-engineer.sh
+++ scripts/applications-labs-engineer.sh
@@ -2,3 +2,7 @@ echo
 echo "Configuring direnv with bash-it"
 brew install direnv
 cp files/direnv.bash ~/.bash_it/custom/direnv.bash
+
+source ${MY_DIR}/ruby.sh
+source ${MY_DIR}/node.sh
+source ${MY_DIR}/node-packages.sh
