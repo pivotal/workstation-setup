29add2ec3187fef74ca6c9feae759478786a355a
cjameson@pivotal.io <cjameson@pivotal.io>
2017-04-20 12:23:30 -0700

    Tolerate brew cask install failures

--- scripts/applications-data-engineer.sh
+++ scripts/applications-data-engineer.sh
@@ -1,3 +1,7 @@
+# All these applications are independent, so if one
+# fails to install, don't stop.
+set +e
+
 echo
 echo "Installing Data R&D Engineer applications"
 
@@ -24,7 +28,9 @@ brew cask install docker
 # Docker Bash Completion
 # Reference https://docs.docker.com/docker-for-mac/
 pushd /usr/local/etc/bash_completion.d
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
 popd
+
+set -e
