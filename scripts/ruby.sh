d8464bdaf9e8c7fb258f872c3c0dfe5a18a841ed
GP WLM <gp-workload-manager@pivotal.io>
2017-05-15 11:51:55 -0700

    Install rbenv before bash-it uses it
    The script configuration-bash.sh enabled the rbenv plugin on bash-it.
    This change guarantees that it is installed before this happens.
    
    Previously, rbenv would be installed as a later step in the lab-engineer
    script which would call ruby.sh. But for the data-engineer script this
    did not happen.

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -2,7 +2,6 @@ echo
 echo "Installing Ruby tools and Ruby 2.3.1"
 cp files/.irbrc ~/.irbrc
 brew install readline
-brew install rbenv
 eval "$(rbenv init -)"
 rbenv install 2.3.1 --skip-existing
 rbenv global 2.3.1
