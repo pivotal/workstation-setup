a18e49b14fdf135d78c7103e4a3798305c9b56e5
C.J. Jameson <cjameson@pivotal.io>
2017-06-21 10:40:36 -0700

    Stop setting gopath
    We were setting gopath to what's now the default, no longer necessary.
    
    Additionally, applications-data-engineer.sh is the only script that
    currently installs golang.sh ... the usage of golang in
    greenplum-db/gpdb needs a different gopath anyway. So avoiding this
    extra declaration of the default gopath makes it clearer that GPDB
    developers can set their own gopath. gpdb/gpMgmt/go-utils does have
    direnv configuration but JetBrains IDEs (like Gogland) don't listen to
    direnv: they need it in the shell environment.
    
    Signed-off-by: Nadeem Ghani <nghani@pivotal.io>

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -3,8 +3,4 @@ echo "Installing Golang Development tools"
 
 mkdir -p ~/go/src
 brew install go --cross-compile-common
-export GOPATH=$HOME/go
-echo "Adding GOPATH"
-echo 'export GOPATH==$HOME/go' >> ~/.bash_profile
-
 brew cask install gogland
