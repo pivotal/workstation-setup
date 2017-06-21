7b655c0a66c3d7e8585559733f24839998d6b5ac
Nadeem Ghani <nghani@pivotal.io>
2017-06-21 11:11:51 -0700

    Include Gogland IDE prefs as part of setup
    Adopts a pattern that issue #121 decries...
    
    Signed-off-by: C.J. Jameson <cjameson@pivotal.io>

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -4,3 +4,13 @@ echo "Installing Golang Development tools"
 mkdir -p ~/go/src
 brew install go --cross-compile-common
 brew cask install gogland
+
+echo
+echo "Setting up Gogland preferences from pivotal_ide_prefs..."
+pushd ~/workspace
+rm -rf pivotal_ide_prefs
+git clone https://github.com/pivotal/pivotal_ide_prefs.git
+pushd pivotal_ide_prefs/cli/
+./bin/ide_prefs install --ide=gogland
+popd
+popd
