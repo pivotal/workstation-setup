8e9ab7589a751e91da93c436dab368efbfe696a3
C.J. Jameson <cjameson@pivotal.io>
2017-08-02 16:55:46 -0700

    Install bosh cli bosh-init bbl
    The only package we don't install from cloudfoundry
    homebrew tap is 'credhub-cli'
    
    Signed-off-by: Goutam Tadi <gtadi@pivotal.io>

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -1,4 +1,5 @@
 echo
 echo "Installing Cloud Foundry Command-line Interface"
 brew tap cloudfoundry/tap
-brew install cf-cli
+brew install cf-cli bosh-cli bosh-init bbl
+ln -s /usr/local/bin/bosh2 /usr/local/bin/bosh
