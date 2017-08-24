039b40fdf2177e2d42d89f2a02c80094c31e6322
ams340 <aaronstreiter@gmail.com>
2017-08-24 10:45:59 -0400

    Remove loginitems from configurations script

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -1,17 +1,11 @@
 echo
-# prereqs
-brew tap OJFord/formulae
-brew install loginitems
-
 echo "Configuring iTerm"
 cp files/com.googlecode.iterm2.plist ~/Library/Preferences
 
 echo "Configuring ShiftIt"
-loginitems -a "ShiftIt" # Start on login
 open /Applications/ShiftIt.app
 
 echo "Configuring FlyCut"
-loginitems -a "Flycut" # Start at login
 open /Applications/Flycut.app
 
 echo
