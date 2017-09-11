9e8bf1fde1bafc7ed52bdd1bb74ce8357403b942
Todd Sedano <tsedano@pivotal.io>
2017-09-11 13:58:43 -0700

    Guard against pre-installed intellij
    Solves https://github.com/pivotal/workstation-setup/issues/97

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,7 +1,7 @@
 echo
 echo "Installing Java Development tools"
 brew cask install java
-brew cask install intellij-idea
+brew cask install intellij-idea --force # guard against pre-installed intellij
 brew install maven
 brew install gradle
 brew install springboot
