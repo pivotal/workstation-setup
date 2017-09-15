743acb091b16d34d61e107285c8e1850ecec343e
Todd Sedano <tsedano@pivotal.io>
2017-09-15 15:14:51 -0700

    Extracts docker to docker.sh

---
+++ scripts/docker.sh
@@ -0,0 +1,15 @@
+# Don't stop if docker fails
+set +e
+
+# Docker
+brew cask install docker
+
+# Docker Bash Completion
+# Reference https://docs.docker.com/docker-for-mac/
+pushd /usr/local/etc/bash_completion.d
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion
+  ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
+popd
+
+set -e
