d8e4bfa99848f8e219d21aa85439ebcbc580a376
Ben Christel <bchristel@pivotal.io>
2017-10-04 10:43:02 -0700

    Refactor git_prompt_info and fix git-together setup steps

--- scripts/finished.sh
+++ scripts/finished.sh
@@ -11,12 +11,12 @@ echo "sudo scutil --set HostName newname"
 
 echo
 echo "You can use 'git pair' (enabled) 'git duet' (enabled) or 'git with' aka git-together (installed but not enabled)"
-echo "To enable git together"
-echo "'git=git-together' >> ~/.bash_profile"
+echo "To enable git together:"
+echo "echo 'alias git=git-together' >> ~/.bash_profile"
+echo "and then follow the configuration instructions at https://github.com/kejadlen/git-together"
 
 echo
 echo "After checking the above output for any problems, start a new iTerm session to make use of all the installed tools."
 echo "Rebooting is only necessary for keyboard repeat settings to work."
 
 echo
-
