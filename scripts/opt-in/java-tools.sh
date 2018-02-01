8054f4ef0b0d4f042da6d303956f6e9faad8f871
Florent Biville <fbiville@pivotal.io>
2018-02-01 11:46:27 +0100

    Install Maven Deluxe instead of just Maven
    Signed-off-by: Yoann Galentin <yoann.galentin@orange.com>

--- scripts/opt-in/java-tools.sh
+++ scripts/opt-in/java-tools.sh
@@ -1,7 +1,8 @@
 echo
 echo "Installing Java Development tools"
 brew cask install intellij-idea --force # guard against pre-installed intellij
-brew install maven
+brew tap jcgay/jcgay
+brew install maven-deluxe
 brew install gradle
 brew install springboot
 
