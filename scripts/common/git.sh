4f45f74d07306abdea0eb2672fb85c125ceda9c3
Todd Sedano <tsedano@pivotal.io>
2018-03-13 15:02:53 -0700

    Adds git-hooks-core for detecting credential leaks
    This fixes #189

--- scripts/common/git.sh
+++ scripts/common/git.sh
@@ -17,3 +17,15 @@ echo
 echo "Setting global Git configurations"
 git config --global core.editor /usr/bin/vim
 git config --global transfer.fsckobjects true
+
+echo
+echo "Installing git hooks for cred-alert"
+# for more information see https://github.com/pivotal-cf/git-hooks-core
+git clone https://github.com/pivotal-cf/git-hooks-core $HOME/workspace/git-hooks-core
+git config --global --add core.hooksPath $HOME/workspace/git-hooks-core
+# install cred-alert-cli
+os_name=$(uname | awk '{print tolower($1)}')
+curl -o cred-alert-cli \
+  https://s3.amazonaws.com/cred-alert/cli/current-release/cred-alert-cli_${os_name}
+chmod 755 cred-alert-cli
+mv cred-alert-cli /usr/local/bin # <= or other directory in ${PATH}
