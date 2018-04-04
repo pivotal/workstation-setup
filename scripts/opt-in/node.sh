a674c6d7970ecff636bccf25d088e5e98d256b7c
Voom Pair <pair@Helionix.local>
2018-04-04 16:58:10 -0700

    use bash-based NVM rather than homebrew NVM

--- scripts/opt-in/node.sh
+++ scripts/opt-in/node.sh
@@ -1,7 +1,12 @@
 echo
 echo "Installing NodeJS"
 
-brew install nvm
+bash-it enable plugin nvm
+# brew install nvm (The NVM website suggests there are some problems with the homebrew version; don't use it)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 brew install node
 brew install yarn
 
