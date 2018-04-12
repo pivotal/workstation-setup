e0a99e726b57611f50481edf15c0978b9a0a7fe7
Todd Sedano <tsedano@pivotal.io>
2018-04-12 15:45:35 -0700

    Removes virtualbox

--- setup.sh
+++ setup.sh
@@ -18,7 +18,6 @@ source ${MY_DIR}/scripts/common/configuration-bash.sh
 # Place any applications that require the user to type in their password here
 brew tap caskroom/cask
 brew cask install github
-brew cask install virtualbox
 brew cask install zoomus
 
 source ${MY_DIR}/scripts/common/git.sh
