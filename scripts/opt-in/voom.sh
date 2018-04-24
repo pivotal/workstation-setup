484856fff6ce33b6b2887f9e07ac5987fe29599b
Evan Tahler <evan.tahler@voom.flights>
2018-04-24 12:03:07 -0700

    install google drive file stream and a cool helicoper photo

--- scripts/opt-in/voom.sh
+++ scripts/opt-in/voom.sh
@@ -19,5 +19,8 @@ brew services start postgresql
 brew services start redis
 brew services start chromedriver
 
+# Voom stores files on Google Drive
+brew install google-drive-file-stream
+
 # JAMF
-echo "!!! DO NOT FORGET TO ENROLL THIS MACHINE IN JAMF !!!" 
+echo "!!! DO NOT FORGET TO ENROLL THIS MACHINE IN JAMF !!!"
