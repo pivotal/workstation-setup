c3ad6f7489ff1a0b400178d9ef6826b0ee65dee9
James Hart and Rob Head <pair+james+rob@voom.flights>
2018-09-05 10:10:38 -0700

    Convert remaining brew installs to conditional installs

--- scripts/common/unix.sh
+++ scripts/common/unix.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+source scripts/helpers/brew.sh
 
 echo
 echo "Installing utilities for unix development"
 
 # For users of unixes
-brew install pstree
+brew_install_if_missing pstree
 
 # For developers of shell scripts
-brew install jq
+brew_install_if_missing jq
