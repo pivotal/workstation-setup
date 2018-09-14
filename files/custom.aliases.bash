04d308cb2a8f8a1c5aceb4f1a918c308bc6f6328
Brian Artiaco and Erika Pacheco <brian.artiaco+erika@voom.flights>
2018-09-14 09:54:26 -0700

    Change daily alias

--- files/custom.aliases.bash
+++ files/custom.aliases.bash
@@ -1,7 +1,7 @@
 # Vüm Specific Aliases
 
-alias daily='bundle && yarn && lint && specs && heroku local'
-alias lint='rubocop && npm run eslint .'
+alias daily='bundle && yarn && rails voom:open voom:refresh && lint && specs && heroku local'
+alias lint='rubocop && yarn eslint .'
 alias specs='rails db:migrate && rails db:seed && rails db:test:prepare && rails parallel:prepare && rails parallel:spec'
 
 
