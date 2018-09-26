607e2c7e20bc76054f629581e4978d5671fbcf2d
Evan Tahler and James Hart <pair+evan+james@voom.flights>
2018-09-26 14:18:15 -0700

    Install version 8 of node.

--- scripts/opt-in/node.sh
+++ scripts/opt-in/node.sh
@@ -1,16 +1,18 @@
 source scripts/helpers/brew.sh
 
 echo
-echo "Installing most recent version of NodeJS"
+echo "Installing version of NodeJS"
 
-bash-it enable plugin nvm
 # brew install nvm (The NVM website suggests there are some problems with the homebrew version; don't use it)
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+bash-it enable plugin nvm
 
 brew_install_if_missing node
 brew_install_if_missing yarn
 
 nvm install v8
 nvm alias default v8
+npm install yarn -g
