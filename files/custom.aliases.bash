ccac9eaf319aa6b2dd291b6ac7a27d455d136e3e
Antoine Dreessen and James Hart <pair+antoine.dreessen+james.hart@voom.flights>
2019-03-26 16:34:40 -0700

    Clean up the db.

--- files/custom.aliases.bash
+++ files/custom.aliases.bash
@@ -2,6 +2,7 @@
 
 alias daily='bundle && yarn && rails voom:open voom:refresh && lint && specs && heroku local'
 alias lint='rubocop && yarn eslint .'
+alias cleandb='rails db:reset voom:open voom:refresh voom:create_test_promotion'
 alias specs='rails db:migrate && rails db:seed && rails db:test:prepare && rails parallel:prepare && rails parallel:spec'
 
 
