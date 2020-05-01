372e67d2fffef79dc91b88b2d7721a6cb929dd6e
Santiago Vasquez <santiagovm@users.noreply.github.com>
2020-05-01 06:17:58 -0400

    Remove ruby version from prompt

--- files/bobby_pivotal/bobby_pivotal.theme.bash
+++ files/bobby_pivotal/bobby_pivotal.theme.bash
@@ -15,7 +15,7 @@ RVM_THEME_PROMPT_SUFFIX="|"
 # This Pivotal's customization of the bobby theme
 
 function prompt_command() {
-    PS1="\n$(battery_char) ${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
+    PS1="\n$(battery_char) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
 }
 
 PROMPT_COMMAND=prompt_command;
