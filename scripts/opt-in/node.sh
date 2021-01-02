4b34e1ce1710fcff218372fb57988bba11fc348f
edohring <edohring@wellesley.edu>
2021-01-02 10:17:11 -0500

    Include webstorm in node setup

--- scripts/opt-in/node.sh
+++ scripts/opt-in/node.sh
@@ -10,3 +10,11 @@ npm install --global yo
 npm install --global webpack
 npm install --global grunt-cli
 npm install --global gulp-cli
+
+# guard against preinstalled webstorm
+brew install --cask webstorm --force
+
+source ${MY_DIR}/scripts/common/download-pivotal-ide-prefs.sh
+pushd ~/workspace/pivotal_ide_prefs/cli
+./bin/ide_prefs install --ide=webstorm
+popd
