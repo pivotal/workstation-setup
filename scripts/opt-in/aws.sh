734c848fd3410c5a2c045e6366c8e708b41196c9
pgoodwin <phil.goodwin@rxrevu.com>
2021-01-29 11:13:39 -0800

    Copy in AWS example files

--- scripts/opt-in/aws.sh
+++ scripts/opt-in/aws.sh
@@ -2,3 +2,7 @@ echo
 echo "Installing the AWS cli"
 
 brew install awscli
+
+mkdir ~/.aws
+cp ${WORKSTATION_SETUP_HOME}/files/aws_config.example ~/.aws/aws_config
+cp ${WORKSTATION_SETUP_HOME}/files/aws_credentials.example ~/.aws/aws_credentials
