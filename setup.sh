b43854ba618b4a8c090af30eb39365f16848dc49
Erica Dohring <dohring.erica@gmail.com>
2021-01-30 08:27:29 -0500

    Begin migration from bash-it to Oh My Zsh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ fi
 
 # Note: Homebrew needs to be set up first
 source ${MY_DIR}/scripts/common/homebrew.sh
-source ${MY_DIR}/scripts/common/configuration-bash.sh
+source ${MY_DIR}/scripts/common/configuration-zsh.sh
 
 # Place any applications that require the user to type in their password here
 brew install --cask github
