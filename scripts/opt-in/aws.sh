01b98496bcd1ca11f1475f7f8be986b6568ec5b2
pgoodwin <phil.goodwin@rxrevu.com>
2021-02-01 15:15:30 -0800

    Remove extra square bracket...

--- scripts/opt-in/aws.sh
+++ scripts/opt-in/aws.sh
@@ -3,14 +3,14 @@ echo "Installing the AWS cli"
 
 brew install awscli
 
-if [[ ! -d ~/.aws ]]] ; then
+if [[ ! -d ~/.aws ]] ; then
   mkdir ~/.aws
 fi
 
-if [[ ! -f ~/.aws/config ]]] ; then
+if [[ ! -f ~/.aws/config ]] ; then
 cp ${WORKSTATION_SETUP_HOME}/files/aws_config.example ~/.aws/config
 fi
 
-if [[ ! -f ~/.aws/credentials ]]] ; then
+if [[ ! -f ~/.aws/credentials ]] ; then
 cp ${WORKSTATION_SETUP_HOME}/files/aws_credentials.example ~/.aws/credentials
 fi
