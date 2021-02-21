cb64d6f4fdc70c4043dc169ea53b1139b6a70e2f
pgoodwin <phil.goodwin@rxrevu.com>
2021-02-21 12:19:17 -0800

    Removed Pivotal specific cred-alert cli

--- scripts/common/git.sh
+++ scripts/common/git.sh
@@ -27,25 +27,3 @@ git config --global transfer.fsckobjects true
 mkdir -p ~/.git_templates
 git config --global init.templateDir ~/.git_templates
 echo "ref: refs/heads/main" > ~/.git_templates/HEAD
-
-HOOKS_DIRECTORY=$HOME/workspace/git-hooks-core
-if [ ! -d $HOOKS_DIRECTORY ]; then
-  echo
-  echo "Installing git hooks for cred-alert"
-  # for more information see https://github.com/pivotal-cf/git-hooks-core
-  git clone https://github.com/pivotal-cf/git-hooks-core $HOOKS_DIRECTORY
-  git config --global --add core.hooksPath $HOOKS_DIRECTORY
-else
-  echo
-  echo "Updating git-hooks for cred-alert"
-  pushd $HOOKS_DIRECTORY
-  git pull -r
-  popd
-fi
-
-# install cred-alert-cli
-os_name=$(uname | awk '{print tolower($1)}')
-curl -o cred-alert-cli \
-  https://s3.amazonaws.com/cred-alert/cli/current-release/cred-alert-cli_${os_name}
-chmod 755 cred-alert-cli
-mv cred-alert-cli /usr/local/bin # <= or other directory in ${PATH}
