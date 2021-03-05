3354d8e68bed7104854e25457a39bdc95b182d88
Sarah Michaelson <sarah@rxrevu.com>
2021-03-05 09:27:13 -0700

    Update installation of spring boot

--- scripts/opt-in/java-tools.sh
+++ scripts/opt-in/java-tools.sh
@@ -4,7 +4,8 @@ brew install --cask intellij-idea --force # guard against pre-installed intellij
 brew tap jcgay/jcgay
 brew install maven-deluxe
 brew install gradle
-brew install springboot
+brew tap spring-io/tap
+brew install spring-boot
 
 source ${WORKSTATION_SETUP_HOME}/scripts/common/download-pivotal-ide-prefs.sh
 pushd ~/workspace/pivotal_ide_prefs/cli
