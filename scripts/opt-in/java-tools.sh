4cb9d908155398486b8a9856e6be663a7765ab97
Sarah Michaelson <skmichaelson@Sarahs-MacBook-Pro.local>
2021-03-08 18:03:43 -0700

    Update installation of Maven and Spring Boot

--- scripts/opt-in/java-tools.sh
+++ scripts/opt-in/java-tools.sh
@@ -2,7 +2,7 @@ echo
 echo "Installing Java Development tools"
 brew install --cask intellij-idea --force # guard against pre-installed intellij
 brew tap jcgay/jcgay
-brew install maven-deluxe
+brew install maven
 brew install gradle
 brew tap spring-io/tap
 brew install spring-boot
