8ede95e495a35b637dfcc529fc777df0494b8acd
jtzero ? <jtzero511@gmail.com>
2021-07-07 13:00:26 -0600

    removes vim config install

--- scripts/common/configurations.sh
+++ scripts/common/configurations.sh
@@ -5,13 +5,3 @@ cp files/com.googlecode.iterm2.plist ~/Library/Preferences
 echo "Configuring ShiftIt"
 open /Applications/ShiftIt.app
 
-echo
-echo "Installing vim configuration"
-pushd ~/
-if [ ! -d ~/.vim ]; then
-    git clone https://github.com/pivotal/vim-config.git ~/.vim
-    ~/.vim/bin/install
-fi
-popd
-
-
