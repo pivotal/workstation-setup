d126500dffec02acf891464de0c108b139f3d4d6
jtzero ? <jtzero511@gmail.com>
2021-07-07 12:58:10 -0600

    adding conditional to omz setup

--- scripts/common/oh-my-zsh.sh
+++ scripts/common/oh-my-zsh.sh
@@ -1,9 +1,13 @@
-echo
-echo "Installing oh_my_zsh..."
+if [ -z "${ZSH}" ]; then
+  echo
+  echo "Installing oh_my_zsh..."
 
-curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
-# switch the shell to zsh
-if [[ "${CHSH}" != "no" ]]; then
-  chsh -s $(which zsh)
+  # switch the shell to zsh
+  if [[ "${CHSH}" != "no" ]]; then
+    chsh -s $(which zsh)
+  fi
+else
+  printf "ZSH variable already set not installing omz\n"
 fi
