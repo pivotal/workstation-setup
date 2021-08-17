9ec5ddae941c41edfb529f6cdbd12c5afcdfd51f
Bastian Doetsch <bdoetsch@vmware.com>
2021-08-17 14:47:25 +0200

    install minikube & docker when choosing kubernetes option

--- scripts/opt-in/kubernetes.sh
+++ scripts/opt-in/kubernetes.sh
@@ -1,12 +1,19 @@
 echo
 echo "Installing common Kubernetes tooling"
 
+# install docker to have a container runtime for kubernetes/containers
+source ${MY_DIR}/scripts/opt-in/docker.sh
+
+# install minikube to have a kubernetes installation
+brew install minikube
 brew install kubectl
+
+
 # Shell completion
 FILE=~/.zshrc
 if [[ -f "$FILE" ]]; then
     echo "$FILE exists proceeding."
-else 
+else
     echo "$FILE does not exist, creating."
     touch $FILE
 fi
@@ -16,3 +23,6 @@ echo 'alias k=kubectl' >>~/.zshrc
 echo 'complete -F __start_kubectl k' >>~/.zshrc
 echo 'autoload -Uz compinit' >>~/.zshrc
 echo 'compinit' >>~/.zshrc
+
+echo 'eval $(minikube completion $(basename $SHELL))' >> ~/.zshrc.local
+
