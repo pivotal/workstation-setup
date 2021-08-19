2aa58c40bbd21c299655bea0b9c1e5c9b2c8f8a6
Joe Moore <joe.moore@gmail.com>
2021-08-19 01:55:16 -0700

    Split cred-alert from git script
    - Remove from git.sh
    - Create new opt-in script for cred-alert
    - Add to setup.sh commented out (optional) with note

--- scripts/common/git.sh
+++ scripts/common/git.sh
@@ -25,24 +25,3 @@ mkdir -p ~/.git_templates
 git config --global init.templateDir ~/.git_templates
 echo "ref: refs/heads/main" > ~/.git_templates/HEAD
 
-HOOKS_DIRECTORY=$HOME/workspace/git-hooks-core
-if [ ! -d $HOOKS_DIRECTORY ]; then
-  echo
-  echo "Installing git hooks for cred-alert"
-  # for more information see https://github.com/pivotal-cf/git-hooks-core
-  git clone https://github.com/pivotal-cf/git-hooks-core $HOOKS_DIRECTORY
-  git config --global --add core.hooksPath $HOOKS_DIRECTORY
-else
-  echo
-  echo "Updating git-hooks for cred-alert"
-  pushd $HOOKS_DIRECTORY
-  git pull -r
-  popd
-fi
-
-# install cred-alert-cli
-os_name=$(uname | awk '{print tolower($1)}')
-curl -L -o cred-alert-cli \
-  https://github.com/pivotal-cf/cred-alert/releases/latest/download/cred-alert-cli_${os_name}
-chmod 755 cred-alert-cli
-mv cred-alert-cli /usr/local/bin # <= or other directory in ${PATH}
