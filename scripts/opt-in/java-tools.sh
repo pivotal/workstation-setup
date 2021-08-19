d4fdd289cb60e2f1c451a017680468ec887d9b79
Bastian Doetsch <bdoetsch@vmware.com>
2021-08-19 10:41:25 +0200

    re-enable maven deluxe, tap works now

--- scripts/opt-in/java-tools.sh
+++ scripts/opt-in/java-tools.sh
@@ -2,8 +2,8 @@ echo
 echo "Installing Java Development tools"
 brew install --cask intellij-idea --force # guard against pre-installed intellij
 # maven deluxe URL / download does not work for me
-#brew tap jcgay/jcgay
-#brew install maven-deluxe
+brew tap jcgay/jcgay
+brew install maven-deluxe
 brew install gradle
 brew install springboot
 
