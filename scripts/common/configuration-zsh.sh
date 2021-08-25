694df4bef4b1fdefb470f6aed87a28d19f894aad
Joe Moore <joe.moore@gmail.com>
2021-08-25 11:18:32 -0600

    installing oh-my-zsh should not crash
    - adds --unattended

--- scripts/common/configuration-zsh.sh
+++ scripts/common/configuration-zsh.sh
@@ -5,6 +5,6 @@ brew install coreutils
 brew install watch
 
 echo "Installing Oh My Zsh"
-ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
 brew install direnv
