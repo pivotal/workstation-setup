9f72ee71411500bd2cfac284ff6ba982d88b93fd
Bastian Doetsch <bdoetsch@vmware.com>
2021-08-28 14:06:06 +0200

    set parser for SnipMate VIM plugin to get rid of deprecation msg

--- scripts/common/configurations.sh
+++ scripts/common/configurations.sh
@@ -4,6 +4,7 @@ pushd ~/
 if [ ! -d ~/.vim ]; then
     git clone https://github.com/pivotal/vim-config.git ~/.vim
     ~/.vim/bin/install
+    echo "let g:snipMate = { 'snippet_version' : 1 }" >> ~/.vim/vimrc
 fi
 popd
 
