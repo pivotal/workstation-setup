0eed691f92fd6c803f1b720192ea913ad22d269b
Joe Moore <joe.moore@gmail.com>
2021-09-01 15:16:46 -0600

    Make Spring Boot optional
    - Was automatically installed when installing java

--- scripts/opt-in/java-tools.sh
+++ scripts/opt-in/java-tools.sh
@@ -5,9 +5,6 @@ brew tap jcgay/jcgay
 brew install maven-deluxe
 brew install gradle
 
-brew tap spring-io/tap
-brew install spring-boot
-
 source ${MY_DIR}/scripts/common/download-jetbrains-ide-prefs.sh
 pushd ~/workspace/jetbrains-ide-prefs/cli
 ./bin/ide_prefs install --ide=intellij
--- scripts/opt-in/java.sh
+++ scripts/opt-in/java.sh
@@ -2,8 +2,7 @@ echo
 echo "Installing most recent version of OpenJDK"
 brew install openjdk
 
-# Configure opensjdk as instructed by 
-# brew info openjdk
+# Configure opensjdk as instructed by 'brew info openjdk'
 sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshenv
 echo 'export CPPFLAGS="-I/usr/local/opt/openjdk/include"' >> ~/.zshenv
