5a7775e7c1c18e8acef1eeb7409c8c95325149f2
Bastian Doetsch <bdoetsch@vmware.com>
2021-09-04 09:57:24 +0200

    quote stuff, remove non-working code in basti.sh

--- scripts/opt-in/basti.sh
+++ scripts/opt-in/basti.sh
@@ -37,25 +37,19 @@ then
   brew install arq
 
   # install python & java & lima
-  source $MY_DIR/scripts/opt-in/python.sh
-  source $MY_DIR/scripts/opt-in/java.sh
-  source $MY_DIR/scripts/opt-in/containerization.sh
+  source "$MY_DIR/scripts/opt-in/python.sh"
+  source "$MY_DIR/scripts/opt-in/java.sh"
+  source "$MY_DIR/scripts/opt-in/containerization.sh"
 
   # mac app store installations
-  mas purchase $(mas search "Unsplash Wallpapers" | head -1 | cut -c1-12)
-  mas purchase $(mas search "Be Focused" | head -1 | cut -c1-12)
-  mas purchase $(mas search "StuffIt Expander" | head -1 | cut -c1-12)
-  mas purchase $(mas search "HP Smart for Desktop" | head -1 | cut -c1-12)
-  mas purchase $(mas search "HP Easy Scan" | head -1 | cut -c1-12)
-
-  # download iriun
-  curl -o iriun.pkg https://1758658189.rsc.cdn77.org/IriunWebcam-2.5.1.pkg && open iriun.pkg && rm iriun.pkg
-
-  # configure apps
-  open "/Applications/Utilities/Adobe\ Creative\ Cloud/ACC/Creative\ Cloud.app"
+  mas purchase "$(mas search "Unsplash Wallpapers" | head -1 | cut -c1-12)"
+  mas purchase "$(mas search "Be Focused" | head -1 | cut -c1-12)"
+  mas purchase "$(mas search "StuffIt Expander" | head -1 | cut -c1-12)"
+  mas purchase "$(mas search "HP Smart for Desktop" | head -1 | cut -c1-12)"
+  mas purchase "$(mas search "HP Easy Scan" | head -1 | cut -c1-12)"
 
   # configure dock
-  source ${MY_DIR}/scripts/common/configuration-osx.sh
+  source "${MY_DIR}/scripts/common/configuration-osx.sh"
   dockutil --list | awk -F\t '{print "dockutil --remove \""$1"\" --no-restart"}' | sh
   dockutil --add /System/Applications/Mail.app --no-restart
   dockutil --add /Applications/Safari.app --no-restart
