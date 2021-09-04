aad091d929986a232d13636f7ad2566cf9270813
Bastian Doetsch <bdoetsch@vmware.com>
2021-09-04 11:13:39 +0200

    make java-tools.sh a real script and explicitly don't fail on single commands

--- scripts/opt-in/java-tools.sh
+++ scripts/opt-in/java-tools.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
+set +e
 echo
 echo "Installing Java Development tools"
 brew install --cask intellij-idea --force # guard against pre-installed intellij
-# maven deluxe URL / download does not work for me
 brew tap jcgay/jcgay
 brew install maven-deluxe
 brew install gradle
@@ -9,7 +10,8 @@ brew tap spring-io/tap
 brew uninstall springboot
 brew install spring-boot
 
-source ${MY_DIR}/scripts/common/download-jetbrains-ide-prefs.sh
-pushd ~/workspace/jetbrains-ide-prefs/cli
-./bin/ide_prefs install --ide=intellij
+source "${MY_DIR}/scripts/common/download-jetbrains-ide-prefs.sh"
+pushd ~/workspace/jetbrains-ide-prefs/cli 1>/dev/null
+  ./bin/ide_prefs install --ide=intellij
 popd
+set -e
