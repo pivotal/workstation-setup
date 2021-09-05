4e8c590d31f4ad6893ba3cb8436a82f6851302dc
Bastian Doetsch <bdoetsch@vmware.com>
2021-09-05 11:45:44 +0200

    make dockutil more resilient by quoting

--- scripts/opt-in/basti.sh
+++ scripts/opt-in/basti.sh
@@ -51,25 +51,25 @@ then
   mas purchase 1147396723 # WhatsApp
 
   # configure dock
-  dockutil --list | awk -F\t '{print "dockutil --remove \""$1"\" --no-restart"}' | sh
-  dockutil --add /System/Applications/Mail.app --no-restart
-  dockutil --add /Applications/Safari.app --no-restart
-  dockutil --add /System/Applications/Calendar.app --no-restart
-  dockutil --add /System/Applications/Contacts.app --no-restart
-  dockutil --add /Applications/Slack.app --no-restart
-  dockutil --add /Applications/iTerm.app --no-restart
-  dockutil --add /Applications/IntelliJ\ IDEA.app --no-restart
-  dockutil --add /Applications/Miro.app --no-restart
-  dockutil --add /System/Applications/FaceTime.app --no-restart
-  dockutil --add /System/Applications/Messages.app --no-restart
-  dockutil --add /Applications/Signal.app --no-restart
-  dockutil --add /Applications/Adobe\ Lightroom\ CC/Adobe\ Lightroom.app --no-restart
-  dockutil --add /Applications/Microsoft\ Excel.app --no-restart
-  dockutil --add /Applications/Microsoft\ Word.app --no-restart
-  dockutil --add /Applications/Microsoft\ OneNote.app --no-restart
-  dockutil --add /System/Applications/Notes.app --no-restart
-  dockutil --add /System/Applications/Reminders.app --no-restart
-  dockutil --add /System/Applications/Utilities/Activity\ Monitor.app --no-restart
+  dockutil --list | awk -F$(printf '\t') '{print "dockutil --remove \""$1"\" --no-restart"}' | sh
+  dockutil --add "/System/Applications/Mail.app" --no-restart
+  dockutil --add "/Applications/Safari.app" --no-restart
+  dockutil --add "/System/Applications/Calendar.app" --no-restart
+  dockutil --add "/System/Applications/Contacts.app" --no-restart
+  dockutil --add "/Applications/Slack.app" --no-restart
+  dockutil --add "/Applications/iTerm.app" --no-restart
+  dockutil --add "/Applications/IntelliJ IDEA.app" --no-restart
+  dockutil --add "/Applications/Miro.app" --no-restart
+  dockutil --add "/System/Applications/FaceTime.app" --no-restart
+  dockutil --add "/System/Applications/Messages.app" --no-restart
+  dockutil --add "/Applications/Signal.app" --no-restart
+  dockutil --add "/Applications/Adobe Lightroom CC/Adobe Lightroom.app" --no-restart
+  dockutil --add "/Applications/Microsoft Excel.app" --no-restart
+  dockutil --add "/Applications/Microsoft Word.app" --no-restart
+  dockutil --add /"Applications/Microsoft OneNote.app" --no-restart
+  dockutil --add "/System/Applications/Notes.app" --no-restart
+  dockutil --add "/System/Applications/Reminders.app" --no-restart
+  dockutil --add "/System/Applications/Utilities/Activity Monitor.app" --no-restart
   dockutil --add "$HOME/Boxcryptor/OneDrive/Documents" --view auto --display stack --no-restart
   dockutil --add "$HOME/Downloads/" --view auto --display stack
 fi
