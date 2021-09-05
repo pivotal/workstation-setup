61aeb3186b2e5356ed77355ad43da02aceeb3c05
Bastian Doetsch <bdoetsch@vmware.com>
2021-09-05 11:50:30 +0200

    set locale to en_US.UTF-8

--- vagrant/01_start.sh
+++ vagrant/01_start.sh
@@ -4,7 +4,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 TZ=Europe/Berlin
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-
+locale-gen en_US.UTF-8
+echo 'LANG="en_US.UTF-8"' > /etc/default/locale
 apt-get -y update && apt-get -y upgrade
 
 source /home/vagrant/workstation-setup/02_install-base.sh
