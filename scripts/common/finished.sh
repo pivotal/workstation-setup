cb0ce7b17a4f51d3baafaebc3a9b817dc1eba8bb
Liam Morley <lmorley@vmware.com>
2021-09-07 16:15:13 -0400

    Remove reference to git-together
    Signed-off-by: Liam Morley <lmorley@vmware.com>

--- scripts/common/finished.sh
+++ scripts/common/finished.sh
@@ -12,13 +12,6 @@ echo "sudo scutil --set HostName newname"
 echo
 echo "For pair programming, git-duet is installed. https://github.com/git-duet/git-duet"
 echo "To configure your pairs, edit ~/.git-authors"
-echo ""
-echo "To enable git together:"
-echo "echo 'alias git=git-together' >> ~/.bash_profile"
-echo "and then follow the configuration instructions at https://github.com/kejadlen/git-together"
-echo ""
-echo "To use git-author:"
-echo "echo 'export GIT_TOGETHER_NO_SIGNOFF=1' >> ~/.bash_profile"
 
 echo
 echo "After checking the above output for any problems, start a new iTerm session to make use of all the installed tools."
