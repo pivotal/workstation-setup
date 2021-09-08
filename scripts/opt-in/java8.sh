d730cef69336a5380a67afe5bbb01fcfe3e5644a
Joe Moore <joe.moore@gmail.com>
2021-09-08 14:51:52 -0600

    Remove java8 opt-in warning script

--- scripts/opt-in/java8.sh
+++
@@ -1,4 +0,0 @@
-echo
-echo "WARNING: Installing Java 8 is no longer supported"
-echo "         Manual installation can still be performed here:"
-echo "         https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html"
