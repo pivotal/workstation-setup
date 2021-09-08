617ad1167ab78e362a47a8323776a504c9405f60
Bastian Doetsch <bdoetsch@vmware.com>
2021-09-08 11:27:21 +0200

    remove unnecessary copy of sdkman bash file

--- scripts/custom/basti.sh
+++ scripts/custom/basti.sh
@@ -112,7 +112,6 @@ if [[ ! -d "$HOME/.sdkman" ]]; then
   curl -s "https://get.sdkman.io?rcupdate=false" | bash
   # shellcheck disable=SC2016
   echo '[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"' >>~/.zshrc.local
-  cp files/sdkman.bash ~/.bash_it/custom/sdkman.bash
 fi
 
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
