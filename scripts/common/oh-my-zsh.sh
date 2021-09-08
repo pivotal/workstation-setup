b18edea3c65bfa0641e33a3123ffb03c68e17858
Joe Moore <joe.moore@gmail.com>
2021-09-08 14:50:33 -0600

    Ignore oh-my-zsh completion warning.
    - Zsh gets mad when completion scripts are not owned
      by a single users. We often have multiple admin users.
      Thus we ignore this warning.

--- scripts/common/oh-my-zsh.sh
+++ scripts/common/oh-my-zsh.sh
@@ -1,4 +1,19 @@
 echo
 
+# Don't exit if oh-my-zsh fails
+set +e
+
 echo "Installing Oh My Zsh (and don't exit if it's already installed)"
-ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended || true
+ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+
+echo "Ignoring insecure completion-dependent directories."
+echo "Needed when multiple admin users are using oh-my-zsh."
+echo "
+
+# Ignoring insecure completion-dependent directories.
+# Needed when multiple admin users are using oh-my-zsh.
+ZSH_DISABLE_COMPFIX=true
+
+" >> ~/.zshenv
+
+set -e
