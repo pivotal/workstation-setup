f5f5567955cad42362547cd917934f6759951767
Joe Moore <joe.moore@gmail.com>
2021-09-15 17:04:17 -0600

    Don't move dirs unless we need to

--- scripts/common/download-jetbrains-ide-prefs.sh
+++ scripts/common/download-jetbrains-ide-prefs.sh
@@ -1,7 +1,7 @@
-pushd ~/workspace
 if [ ! -d ~/workspace/jetbrains-ide-prefs ]; then
+    pushd ~/workspace
     echo
     echo "Downloading JetBrains IDE preferences"
     git clone https://github.com/professor/jetbrains-ide-prefs.git
+    popd
 fi
-popd
