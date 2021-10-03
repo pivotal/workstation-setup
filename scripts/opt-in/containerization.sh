3d0b69dd4a605ba1f9839d531385d51f85f1ad7e
Bastian Doetsch <bdoetsch@vmware.com>
2021-10-03 10:32:48 +0200

    remove adding lima vm autostart from .zshrc.local as mostly don't needed

--- scripts/opt-in/containerization.sh
+++ scripts/opt-in/containerization.sh
@@ -6,13 +6,4 @@ echo 'Installing Lima instead of docker...'
 brew install lima
 echo 'Starting Lima for the first time...'
 limactl start
-cat <<EOF >> ~/.zshrc.local
-alias docker="lima nerdctl"
-echo
-echo
-echo "Starting Lima VM... please wait..."
-echo
-limactl start
-bashcompinit
-eval "\$(limactl completion bash)"
-EOF
+echo 'alias docker="lima nerdctl"' >> ~/.zshrc.local
