6655add9e04a09e3b0782c5a6e6d9bebee995db7
Bastian Doetsch <bdoetsch@vmware.com>
2021-10-03 10:18:02 +0200

    add custom git config

---
+++ scripts/custom/basti-git.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+git config --global core.editor "$EDITOR"
+git config --global core.autocrlf input
+
+if [[ $(command -v delta) ]]; then
+cat <<EOF >> ~/.gitconfig
+[pager]
+    diff = delta
+    log = delta
+    reflog = delta
+    show = delta
+
+[interactive]
+    diffFilter = delta --color-only --features=interactive
+EOF
+fi
