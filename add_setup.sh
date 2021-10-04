cd2ffc36ff2a839118df1d0225f04448f61c67c8
Adrian Avila and Anna Lewis <pair+adrian+anna@rxrevu.com>
2021-10-04 16:48:34 -0600

    Log when an opt-in setup script fails
    [#179213416]

--- add_setup.sh
+++ add_setup.sh
@@ -6,7 +6,7 @@
 #   - a list of components to install, see scripts/opt-in/ for valid options
 
 # Fail immediately if any errors occur
-set -e
+#set -e
 
 . ./_init.sh
 
@@ -17,7 +17,14 @@ do
     FILE=${WORKSTATION_SETUP_HOME}/scripts/opt-in/${var}.sh
     echo "$FILE"
     if [ -f $FILE ]; then
-        . ${FILE}
+        /bin/bash ${FILE}
+        exit_code=$?
+        if [ $exit_code -ne 0 ]; then
+            echo "*************************"
+            echo "Failed to execute $var"
+            echo "*************************"
+            exit $exit_code
+        fi
     else
        echo "Warning: $var does not appear to be a valid argument. File $FILE does not exist."
     fi
