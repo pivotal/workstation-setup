7d41027630944cde9abbc31d63947c78537481e7
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-11-03 21:51:51 +0000

    Get actual vim location for core editor

--- scripts/common/git.sh
+++ scripts/common/git.sh
@@ -4,7 +4,7 @@ brew install git
 
 echo
 echo "Setting global Git configurations"
-git config --global core.editor /usr/local/bin/vim
+git config --global core.editor "$(brew --prefix)/bin/vim"
 git config --global transfer.fsckobjects true
 
 mkdir -p ~/.git_templates
