cab83cbc824aa3bcaf33c6e7a62e3913bab16274
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:25:52 +0000

    remove dash api browser

--- scripts/common/developer-utilities.sh
+++ scripts/common/developer-utilities.sh
@@ -2,7 +2,6 @@ echo
 
 # Don't exit if any of these fail
 set +e
-  brew install --cask dash # api browser
   brew install --cask postman # api interaction tool
   brew install --cask quicklook-json # OSX tool for viewing JSON
 set -e
