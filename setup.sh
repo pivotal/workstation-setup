881b171b438ff5dabb6b950d7b799c67af4cadb3
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:02:16 +0000

    remove oh-my-zsh installation

--- setup.sh
+++ setup.sh
@@ -30,7 +30,6 @@ fi
 source ${MY_DIR}/scripts/common/homebrew.sh
 
 # Install everything else
-source ${MY_DIR}/scripts/common/oh-my-zsh.sh
 source ${MY_DIR}/scripts/common/editors.sh
 source ${MY_DIR}/scripts/common/git.sh
 source ${MY_DIR}/scripts/common/git-aliases.sh
