bc5ae9763e6aff30dd622099c000886910f7037e
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:16:52 +0000

    adjust git aliases

--- scripts/common/git-aliases.sh
+++ scripts/common/git-aliases.sh
@@ -1,22 +1,15 @@
 echo
 echo "Setting up Git aliases..."
-git config --global alias.gst git status
-git config --global alias.st status
-git config --global alias.di diff
-git config --global alias.co checkout
-git config --global alias.ci commit
-git config --global alias.cp cherry-pick
-git config --global alias.br branch
-git config --global alias.sta stash
-git config --global alias.llog "log --date=local"
-git config --global alias.flog "log --pretty=fuller --decorate"
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-git config --global alias.lol "log --graph --decorate --oneline"
-git config --global alias.lola "log --graph --decorate --oneline --all"
-git config --global alias.blog "log origin/master... --left-right"
-git config --global alias.ds "diff --staged"
-git config --global alias.fixup "commit --fixup"
-git config --global alias.squash "commit --squash"
-git config --global alias.amendit "commit --amend --no-edit"
-git config --global alias.unstage "reset HEAD"
-git config --global alias.rum "rebase master@{u}"
+git config --global alias.gst status
+git config --global alias.gdf diff
+git config --global alias.gco checkout
+git config --global alias.gci commit
+git config --global alias.gscam "git commit -S -a -m"
+git config --global alias.gcan! "commit -S --amend --no-edit -a"
+git config --global alias.gcp cherry-pick
+git config --global alias.gbr branch
+git config --global alias.gsta stash
+git config --global alias.glg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.grb "rebase"
+git config --global alias.grm "rebase master"
+git config --global alias.grim "rebase -i master"
