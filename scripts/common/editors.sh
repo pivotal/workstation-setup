4d4ca547000454dea4d357b9ec836e45b28a32bc
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:01:15 +0000

    only keep VS Code in editors

--- scripts/common/editors.sh
+++ scripts/common/editors.sh
@@ -4,20 +4,8 @@ set +e
 
 echo
 echo "Installing editors"
-echo "Only installing Visual Studio Code and Vim by default"
-echo "Uncomment other editors and rerun if you want them"
+echo "Only installing Visual Studio Code"
 
 brew install --cask visual-studio-code
-brew install vim
-
-echo
-echo "Other editors are available in this script."
-echo "Uncomment what you want."
-# brew install --cask macdown
-# brew install --cask sublime-text
-# brew install --cask textmate
-# brew install --cask macvim
-# brew install --cask jetbrains-toolbox --force # guard against pre-installed jetbrains-toolbox
-# brew install --cask atom
 
 set -e
