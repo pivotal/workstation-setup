0302f297513aecc2b63aac346a8a26826bf59f04
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:43:03 +0000

    remove dock config

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -1,15 +1,6 @@
 echo
 echo 'Customizing OS X configuration'
 
-# set menu clock
-# see http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
-defaults write com.apple.menuextra.clock "DateFormat" 'EEE MMM d  h:mm:ss a'
-killall SystemUIServer
-
-# hide the dock
-defaults write com.apple.dock autohide -bool true
-killall Dock
-
 # fast key repeat rate, requires reboot to take effect
 defaults write ~/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
 defaults write ~/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
