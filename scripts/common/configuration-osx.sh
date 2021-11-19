65be0f517ad20c6de91a055ff538b716289ce47f
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:43:40 +0000

    remove old Photos boot config

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -8,10 +8,6 @@ defaults write ~/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
 # set finder to display full path in title bar
 defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
 
-# stop Photos from opening automatically
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
-#to revert use defaults -currentHost delete com.apple.ImageCapture disableHotPlug
-
 
 # modify appearance of dock: remove standard icons, add chrome and iTerm
 brew install dockutil
