29405029814e26a43393c50b556394e19d5b7564
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:07:22 +0000

    add github cli to git installation

--- scripts/common/git.sh
+++ scripts/common/git.sh
@@ -8,4 +8,7 @@ echo "[Skipped] Setting global Git configurations"
 # Unclear on the relevance of this in git v2+
 # git config --global transfer.fsckobjects true
 
+echo "Installing Github CLI"
+brew install gh
+
 set -e
