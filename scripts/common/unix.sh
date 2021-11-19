80dc5046a0c442e95d3cdd8aaf91643060c56f10
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:30:12 +0000

    remove specialist unix tools

--- scripts/common/unix.sh
+++ scripts/common/unix.sh
@@ -3,13 +3,6 @@
 echo
 echo "Installing utilities for unix development"
 
-# Handy CLI tools
-brew install autojump # navigation helper
-brew install grc # colorize things
-brew install coreutils # GNU core utilities
-brew install watch # watch things
-brew install direnv # unclutter your .profile
-
 # For users of unixes
 brew install pstree
 brew install the_silver_searcher
