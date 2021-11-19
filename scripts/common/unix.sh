c2acd72b0e11b9c1b3e81ed2a87aa30b99924021
Omair Vaiyani <omair@synap.ac>
2021-11-19 17:34:54 +0000

    replace specialist search tool with grep

--- scripts/common/unix.sh
+++ scripts/common/unix.sh
@@ -4,9 +4,10 @@ echo
 echo "Installing utilities for unix development"
 
 # For users of unixes
-brew install pstree
-brew install the_silver_searcher
 brew install wget
+## Install a newer version of grep;
+## Mac OS ships with an old, slower version
+brew install grep
 
 # For developers of shell scripts
 brew install jq # JSON utility
