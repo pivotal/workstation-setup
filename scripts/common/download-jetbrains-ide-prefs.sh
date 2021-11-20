81101c782b24c2065186641911a1bc89ffe8bbe6
Omair Vaiyani <omair@synap.ac>
2021-11-20 18:23:27 +0000

    remove unused script

--- scripts/common/download-jetbrains-ide-prefs.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -d ~/workspace/jetbrains-ide-prefs ]; then
-    pushd ~/workspace
-    echo
-    echo "Downloading JetBrains IDE preferences"
-    git clone https://github.com/professor/jetbrains-ide-prefs.git
-    popd
-fi
