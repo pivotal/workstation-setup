cd8091d675cc30eadc8957fe0565eb43cf93e89b
Omair Vaiyani <omair@synap.ac>
2021-11-20 18:15:38 +0000

    add volta and remove old node tools

--- scripts/common/node.sh
+++ scripts/common/node.sh
@@ -6,15 +6,5 @@ brew install node
 echo
 echo "Installing global NodeJS Packages"
 
-npm install --global yo
-npm install --global webpack
-npm install --global grunt-cli
-npm install --global gulp-cli
-
-# guard against preinstalled webstorm
-brew install --cask webstorm --force
-
-#source ${MY_DIR}/scripts/common/download-jetbrains-ide-prefs.sh
-#pushd ~/workspace/jetbrains-ide-prefs/cli
-#./bin/ide_prefs install --ide=webstorm
-#popd
+# install Volta
+curl https://get.volta.sh | bash
