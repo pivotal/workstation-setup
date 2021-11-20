e0df25cb03f9aa656dc719dee0182c295377505a
Omair Vaiyani <omair@synap.ac>
2021-11-20 18:37:12 +0000

    remove opt-in files that we're unlikely to ever use

--- scripts/opt-in/concourse.sh
+++
@@ -1,4 +0,0 @@
-echo
-echo "Installing Concourse for CI/CD tooling"
-
-brew install --cask fly
--- scripts/opt-in/spring-boot.sh
+++
@@ -1,5 +0,0 @@
-echo
-
-echo "Installing Spring Boot"
-brew tap spring-io/tap
-brew install spring-boot
