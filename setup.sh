53921f945dba42116f4901fe389f647521430f08
xtreme-steve-elliott <elliottst@vmware.com>
2022-05-30 11:33:48 -0400

    Adding flag to allow you to list out the opt-ins

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 #
 # Arguments:
 #   - a list of components to install, see scripts/opt-in/ for valid options
+#   - --list to print out the available opt-ins
 #
 # Environment variables:
 #   - SKIP_ANALYTICS:  Set this to 1 to not send usage data to our Google Analytics account
@@ -12,12 +13,42 @@
 # Fail immediately if any errors occur
 set -e
 
+MY_DIR="$(dirname "$0")"
+OPT_IN_FOLDER="${MY_DIR}/scripts/opt-in"
+
+function has_param() {
+    local flags="$1"
+    shift 1 # pull the flags you're looking for out of the args list
+    for flag in $flags; do
+        for arg in $@; do
+            if [[ $arg == "$flag" ]]; then
+                return 0;
+            fi
+        done
+    done
+    return 1
+}
+
+function present_opt_ins() {
+    shopt -s nullglob
+    local optIns=($OPT_IN_FOLDER/*)
+    shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
+
+    for option in ${optIns[@]}; do
+      echo $option | sed -e "s:\.sh$::" -e "s:${OPT_IN_FOLDER}/::"
+    done
+}
+
+if has_param "--list" "$@"; then
+    present_opt_ins
+    exit
+fi
+
 echo "Caching password..."
 sudo -K
 sudo true;
 clear
 
-MY_DIR="$(dirname "$0")"
 SKIP_ANALYTICS=${SKIP_ANALYTICS:-0}
 if (( SKIP_ANALYTICS == 0 )); then
     clientID=$(od -vAn -N4 -tx  < /dev/urandom)
