9007856d464096fca0dc08d99760e257a5932197
Phil Goodwin <phil@rxrevu.com>
2023-01-09 09:55:56 -0800

    Updating the default Ruby version to 3.1.3

--- _init.sh
+++ _init.sh
@@ -1,7 +1,7 @@
 # Shared setup initialization
 
 export WORKSTATION_SETUP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export DEFAULT_RUBY_VERSION=2.7.3
+export DEFAULT_RUBY_VERSION=3.1.3
 export DEFAULT_PYTHON_VERSION=3.8.5
 export DEFAULT_NODEJS_VERSION=10.15.2
 export DEFAULT_TF_VERSION=1.0.1
