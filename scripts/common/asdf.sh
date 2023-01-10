2bf8d2c004ddaed1d766f1c08094e7f3416cd5ce
Alex Terletskiy <alex@rxrevu.com>
2023-01-10 15:58:18 -0700

    Remove unnecessary -e, it breaks .zshrc

--- scripts/common/asdf.sh
+++ scripts/common/asdf.sh
@@ -1,8 +1,8 @@
   if [[ ! -d ~/.asdf ]]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
-    echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-    echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+    echo '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+    echo '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
     . "$HOME/.asdf/asdf.sh"
     brew install coreutils
     brew install gpg
