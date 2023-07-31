86046daa5b5aaa56bf34082c6046b3d0ef2017e7
Jim Hantz <james.hantz@arrivehealth.com>
2023-07-31 10:56:02 -0400

    Add Apple Silicon symlink to java17 and java16 install scripts
    [#182037979]

--- scripts/opt-in/java.sh
+++ scripts/opt-in/java.sh
@@ -3,3 +3,8 @@ echo "Installing Java 17"
 brew install openjdk@17
 
 source "${WORKSTATION_SETUP_HOME}/scripts/opt-in/java-tools.sh"
+
+# Symlink for Apple Silicon to /opt/homebrew. Symlinks to /usr/local for Intel macs
+sudo ln -sfn $(brew --prefix)/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+
+echo 'export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"' >> ~/.zshrc
--- scripts/opt-in/java16.sh
+++ scripts/opt-in/java16.sh
@@ -4,3 +4,8 @@ brew tap adoptopenjdk/openjdk
 brew install adoptopenjdk16
 
 source "${WORKSTATION_SETUP_HOME}/scripts/opt-in/java-tools.sh"
+
+# Symlink for Apple Silicon to /opt/homebrew. Symlinks to /usr/local for Intel macs
+sudo ln -sfn $(brew --prefix)/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-16.jdk
+
+echo 'export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"' >> ~/.zshrc
